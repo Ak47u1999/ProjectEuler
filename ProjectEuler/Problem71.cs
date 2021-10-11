a8ca4ad968f7f1eb227cb905acaeb1691889bfb3
Ak47u1999 <arpittopper.2017@gmail.com>
2021-10-11 19:27:12 +0530

    Stern Brocot tree

--- ProjectEuler/Problem71.cs
+++ ProjectEuler/Problem71.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Numerics;
 using System.Text;
 
 namespace ProjectEuler
@@ -14,9 +15,10 @@ namespace ProjectEuler
             watch.Start();
             //.............................................
 
-            //Farey's Sequence (Slow use circular property)
+            //Farey's Sequence (Use Stern brocot tree)
 
-            int n = 100000, a = 0, b = 1, c = 1, d = n, p, q;
+            int n = 100, a = 0, b = 1, c = 1, d = n, p, q;
+            BigInteger totalTerms = 1;
             decimal temp;
 
             while (true)
@@ -26,9 +28,10 @@ namespace ProjectEuler
 
                 q = Convert.ToInt32(Math.Floor(temp)) * d - b;
 
+                totalTerms++;
                 if (p == 3 && q == 7)
                 {
-                    Console.WriteLine(c + "/" + d);
+                    Console.WriteLine(c + "/" + d+" totalterms: "+totalTerms);
                     break;
                 }
 
